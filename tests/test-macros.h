34f31a6e0bb2bd0721a9ea81943b1bb9e175f795
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2021-09-22 03:05:22 +0530

    Update test-macros.h

--- tests/test-macros.h
+++ tests/test-macros.h
@@ -13,32 +13,40 @@
 #include <errno.h>
 #include <string.h>
 
-#define ASSERT_ERRNO(expected)                                                                                         \
-	if (errno != expected)                                                                                             \
-	{                                                                                                                  \
-		printf_s("Assertion failed at %s:%d. Expected value of %d but got %d\n", __FILE__, __LINE__, expected, errno); \
-		exit(1);                                                                                                       \
+#define ASSERT_ERRNO(expected)                                                                                       \
+	if (errno != expected)                                                                                           \
+	{                                                                                                                \
+		printf("Assertion failed at %s:%d. Expected value of %d but got %d\n", __FILE__, __LINE__, expected, errno); \
+		exit(1);                                                                                                     \
 	}
 
-#define ASSERT_NULL(pointer)                                                           \
-	if (pointer != NULL)                                                               \
-	{                                                                                  \
-		printf_s("Assertion failed at %s:%d. Pointer not NULL\n", __FILE__, __LINE__); \
-		exit(1);                                                                       \
+#define ASSERT_NULL(pointer)                                                         \
+	if (pointer != NULL)                                                             \
+	{                                                                                \
+		printf("Assertion failed at %s:%d. Pointer not NULL\n", __FILE__, __LINE__); \
+		exit(1);                                                                     \
 	}
 
-#define ASSERT_EQ(actual, expected)                                                                                     \
-	if (actual != expected)                                                                                             \
-	{                                                                                                                   \
-		printf_s("Assertion failed at %s:%d. Expected value of %d but got %d\n", __FILE__, __LINE__, expected, actual); \
-		exit(1);                                                                                                        \
-	}
-
-#define ASSERT_STREQ(actual, expected)                                                                                      \
-	if (strcmp(actual, expected) != 0)                                                                                      \
+#define ASSERT_EQ(actual, expected)                                                                                         \
+	if (actual != expected)                                                                                                 \
 	{                                                                                                                       \
-		printf_s("Assertion failed at %s:%d. Expected value of \n%s\nbut got\n%s\n", __FILE__, __LINE__, expected, actual); \
+		printf("Assertion failed at %s:%d. Expected value of %lld but got %lld\n", __FILE__, __LINE__, (long long)expected, \
+			   (long long)actual);                                                                                          \
 		exit(1);                                                                                                            \
+	}
+
+#define ASSERT_STREQ(actual, expected)                                                                                    \
+	if (strcmp(actual, expected) != 0)                                                                                    \
+	{                                                                                                                     \
+		printf("Assertion failed at %s:%d. Expected value of \n%s\nbut got\n%s\n", __FILE__, __LINE__, expected, actual); \
+		exit(1);                                                                                                          \
+	}
+
+#define ASSERT_MEMEQ(actual, expected, size)                                                                                              \
+	if (memcmp(actual, expected, size) != 0)                                                                                              \
+	{                                                                                                                                     \
+		printf("Assertion failed at %s:%d. Expected value of \n%.*s\nbut got\n%.*s\n", __FILE__, __LINE__, size, expected, size, actual); \
+		exit(1);                                                                                                                          \
 	}
 
 #endif
