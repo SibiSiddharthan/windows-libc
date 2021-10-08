b1d5041b028ed879ccbfa621e5b0fff24336275b
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2021-10-08 22:07:35 +0530

    Implement getcwd using NT functions

--- src/unistd/getcwd.c
+++ src/unistd/getcwd.c
@@ -6,77 +6,104 @@
 */
 
 #include <unistd.h>
-#include <Windows.h>
-#include <internal/error.h>
+#include <stdlib.h>
+#include <wchar.h>
+#include <internal/nt.h>
 
 char *wlibc_getcwd(char *buf, size_t size)
 {
-	if (buf == NULL)
-	{
-		errno = EFAULT;
-		return NULL;
-	}
+	char *return_buffer = NULL;
+	UTF8_STRING u8_cwd;
+	RtlUnicodeStringToUTF8String(&u8_cwd, &(NtCurrentPeb()->ProcessParameters->CurrentDirectory.DosPath), TRUE);
+	// Dospath has a trailing slash remove it
+	u8_cwd.Buffer[u8_cwd.Length - 1] = '\0';
 
-	if (size == 0)
-	{
-		errno = EINVAL;
-		return NULL;
-	}
-
-	DWORD length = GetCurrentDirectoryA(size, buf);
-	if (length > size)
+	if (size != 0 && size < u8_cwd.Length)
 	{
 		errno = ERANGE;
-		return NULL;
+		goto finish;
 	}
 
-	// Convert backslash to forward slash
-	char *temp = buf;
-	while (*buf != '\0')
+	if (buf == NULL)
 	{
-		if (*buf == '\\')
+		if (size == 0)
 		{
-			*buf = '/';
+			return_buffer = (char *)malloc(u8_cwd.Length);
+			memcpy(return_buffer, u8_cwd.Buffer, u8_cwd.Length);
 		}
-		++buf;
+		else // size > u8_cwd.Length
+		{
+			return_buffer = (char *)malloc(size);
+			memcpy(return_buffer, u8_cwd.Buffer, u8_cwd.Length);
+		}
 	}
-	buf = temp;
+	else
+	{
+		if (size == 0)
+		{
+			errno = ERANGE;
+			goto finish;
+		}
+		return_buffer = buf;
+		memcpy(buf, u8_cwd.Buffer, u8_cwd.Length);
+	}
 
-	return buf;
+	for (int i = 0; i < u8_cwd.Length - 1; i++)
+	{
+		if (return_buffer[i] == '\\')
+		{
+			return_buffer[i] = '/';
+		}
+	}
+
+finish:
+	RtlFreeUTF8String(&u8_cwd);
+	return return_buffer;
 }
 
 wchar_t *wlibc_wgetcwd(wchar_t *wbuf, size_t size)
 {
-	if (wbuf == NULL)
-	{
-		errno = EFAULT;
-		return NULL;
-	}
+	PUNICODE_STRING cwd = &(NtCurrentPeb()->ProcessParameters->CurrentDirectory.DosPath);
+	USHORT cwd_length = cwd->Length - (1 * sizeof(wchar_t)); // exclude trailing slash
+	wchar_t *return_buffer = NULL;
 
-	if (size == 0)
-	{
-		errno = EINVAL;
-		return NULL;
-	}
-
-	DWORD length = GetCurrentDirectoryW(size, wbuf);
-	if (length > size)
+	if (size != 0 && size * sizeof(wchar_t) < cwd_length)
 	{
 		errno = ERANGE;
 		return NULL;
 	}
 
-	// Convert backslash to forward slash
-	wchar_t *temp = wbuf;
-	while (*wbuf != L'\0')
+	if (wbuf == NULL)
 	{
-		if (*wbuf == L'\\')
+		if (size == 0)
 		{
-			*wbuf = L'/';
+			return_buffer = (wchar_t *)malloc(cwd_length);
+			memcpy(return_buffer, cwd->Buffer, cwd_length);
 		}
-		++wbuf;
+		else // size > u8_cwd.Length
+		{
+			return_buffer = (wchar_t *)malloc(sizeof(wchar_t) * size);
+			memcpy(return_buffer, cwd->Buffer, cwd_length);
+		}
 	}
-	wbuf = temp;
+	else
+	{
+		if (size == 0)
+		{
+			errno = ERANGE;
+			return NULL;
+		}
+		return_buffer = wbuf;
+		memcpy(wbuf, cwd->Buffer, cwd_length);
+	}
 
-	return wbuf;
+	return_buffer[cwd_length] = L'\0';
+	for (int i = 0; i < cwd_length; i++)
+	{
+		if (return_buffer[i] == L'\\')
+		{
+			return_buffer[i] = L'/';
+		}
+	}
+	return return_buffer;
 }
