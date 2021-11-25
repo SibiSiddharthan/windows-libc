f0ef300c7f0429a1e26d149d9a2ebc49507fbc43
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2021-11-26 02:23:34 +0530

    Add alloca.h header file

---
+++ include/alloca.h
@@ -0,0 +1,11 @@
+/*
+   Copyright (c) 2020-2021 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#ifndef WLIBC_ALLOCA_H
+#define WLIBC_ALLOCA_H
+#include <malloc.h>
+#endif
