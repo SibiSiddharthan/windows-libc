80aff0b04452f6f410b9f5e4244ab6e199be51b3
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2021-12-11 16:33:32 +0530

    Implement chflags family of functions

---
+++ src/sys/stat/chflags.c
@@ -0,0 +1,93 @@
+/*
+   Copyright (c) 2020-2021 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#include <internal/nt.h>
+#include <internal/error.h>
+#include <internal/fcntl.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+int do_chflags(HANDLE handle, uint32_t attributes)
+{
+	NTSTATUS status;
+	IO_STATUS_BLOCK io;
+	FILE_BASIC_INFORMATION basic_info;
+
+	status = NtQueryInformationFile(handle, &io, &basic_info, sizeof(FILE_BASIC_INFORMATION), FileBasicInformation);
+	if(status != STATUS_SUCCESS)
+	{
+		map_ntstatus_to_errno(status);
+		return -1;
+	}
+
+	basic_info.FileAttributes = attributes;
+
+	status = NtSetInformationFile(handle, &io, &basic_info, sizeof(FILE_BASIC_INFORMATION), FileBasicInformation);
+	if(status != STATUS_SUCCESS)
+	{
+		map_ntstatus_to_errno(status);
+		return -1;
+	}
+
+	return 0;
+}
+
+int common_chflags(int dirfd, const char *path, uint32_t attributes, int flags)
+{
+	wchar_t *u16_ntpath = get_absolute_ntpath(dirfd, path);
+	if (u16_ntpath == NULL)
+	{
+		errno = ENOENT;
+		return -1;
+	}
+
+	HANDLE handle = just_open(u16_ntpath, FILE_READ_ATTRIBUTES | FILE_WRITE_ATTRIBUTES, 0, FILE_OPEN,
+							  flags == AT_SYMLINK_NOFOLLOW ? FILE_OPEN_REPARSE_POINT : 0);
+	free(u16_ntpath);
+	if (handle == INVALID_HANDLE_VALUE)
+	{
+		// errno wil be set by just_open
+		return -1;
+	}
+
+	int result = do_chflags(handle, attributes);
+	NtClose(handle);
+	return result;
+}
+
+int wlibc_common_chflags(int dirfd, const char *path, uint32_t attributes, int flags)
+{
+	if (flags != 0 && flags != AT_EMPTY_PATH && flags != AT_SYMLINK_NOFOLLOW)
+	{
+		errno = EINVAL;
+		return -1;
+	}
+
+	if (attributes > S_IA_SETTABLE)
+	{
+		errno = EINVAL;
+		return -1;
+	}
+
+	if (flags != AT_EMPTY_PATH)
+	{
+		VALIDATE_PATH_AND_DIRFD(path, dirfd);
+		return common_chflags(dirfd, path, attributes, flags);
+	}
+	else
+	{
+		enum handle_type type = get_fd_type(dirfd);
+		if (type != FILE_HANDLE && type != DIRECTORY_HANDLE)
+		{
+			errno = EBADF;
+			return -1;
+		}
+
+		HANDLE handle = get_fd_handle(dirfd);
+		return do_chflags(handle, attributes);
+	}
+}
---
+++ tests/sys/stat/test-chflags.c
@@ -0,0 +1,134 @@
+/*
+   Copyright (c) 2020-2021 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#include <test-macros.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int test_chflags()
+{
+	int fd;
+	int status;
+	struct stat statbuf;
+	const char *filename = "t-chflags";
+
+	fd = creat(filename, 0700);
+	ASSERT_SUCCESS(close(fd));
+
+	status = stat(filename, &statbuf);
+	ASSERT_EQ(status, 0);
+	// By default any file created with no attributes has FILE_ATTRIBUTE_ARCHIVE bit set.
+	ASSERT_EQ((statbuf.st_attributes & S_IA_MASK), S_IA_ARCHIVE);
+
+	status = chflags(filename, S_IA_READONLY);
+	ASSERT_EQ(status, 0);
+
+	status = stat(filename, &statbuf);
+	ASSERT_EQ(status, 0);
+	ASSERT_EQ((statbuf.st_attributes & S_IA_MASK), (S_IA_READONLY));
+
+	status = chflags(filename, S_IA_READONLY | S_IA_HIDDEN);
+	ASSERT_EQ(status, 0);
+
+	status = stat(filename, &statbuf);
+	ASSERT_EQ(status, 0);
+	ASSERT_EQ((statbuf.st_attributes & S_IA_MASK), (S_IA_READONLY | S_IA_HIDDEN));
+
+	status = chflags(filename, S_IA_SYSTEM);
+	ASSERT_EQ(status, 0);
+
+	status = stat(filename, &statbuf);
+	ASSERT_EQ(status, 0);
+	ASSERT_EQ((statbuf.st_attributes & S_IA_MASK), (S_IA_SYSTEM));
+
+	ASSERT_SUCCESS(unlink(filename));
+	return 0;
+}
+
+int test_lchflags()
+{
+	int fd;
+	int status;
+	struct stat statbuf;
+	const char *filename = "t-lchflags";
+	const char *filename_symlink = "t-lchflags.sym";
+
+	fd = creat(filename, 0770);
+	ASSERT_SUCCESS(close(fd));
+
+	ASSERT_SUCCESS(symlink(filename, filename_symlink));
+
+	status = stat(filename_symlink, &statbuf);
+	ASSERT_EQ(status, 0);
+	ASSERT_EQ((statbuf.st_attributes & S_IA_MASK), S_IA_ARCHIVE);
+
+	status = lstat(filename_symlink, &statbuf);
+	ASSERT_EQ(status, 0);
+	ASSERT_EQ((statbuf.st_attributes & S_IA_MASK), (S_IA_ARCHIVE | S_IA_REPARSE));
+
+	status = lchflags(filename_symlink, S_IA_HIDDEN);
+	ASSERT_EQ(status, 0);
+
+	status = lstat(filename_symlink, &statbuf);
+	ASSERT_EQ(status, 0);
+	ASSERT_EQ((statbuf.st_attributes & S_IA_MASK), (S_IA_HIDDEN | S_IA_REPARSE));
+
+	// The original file's attributes should not change.
+	status = stat(filename_symlink, &statbuf);
+	ASSERT_EQ(status, 0);
+	ASSERT_EQ((statbuf.st_attributes & S_IA_MASK), S_IA_ARCHIVE);
+
+	ASSERT_SUCCESS(unlink(filename));
+	ASSERT_SUCCESS(unlink(filename_symlink));
+	return 0;
+}
+
+int test_fchflags()
+{
+	int fd;
+	int status;
+	struct stat statbuf;
+	const char *filename = "t-fchflags";
+
+	fd = creat(filename, 0700);
+
+	status = stat(filename, &statbuf);
+	ASSERT_EQ(status, 0);
+	ASSERT_EQ((statbuf.st_attributes & S_IA_MASK), S_IA_ARCHIVE);
+
+	status = fchflags(fd, S_IA_HIDDEN | S_IA_ARCHIVE);
+	ASSERT_EQ(status, 0);
+
+	status = stat(filename, &statbuf);
+	ASSERT_EQ(status, 0);
+	ASSERT_EQ((statbuf.st_attributes & S_IA_MASK), (S_IA_HIDDEN | S_IA_ARCHIVE));
+
+	ASSERT_SUCCESS(close(fd));
+	ASSERT_SUCCESS(unlink(filename));
+	return 0;
+}
+
+void cleanup()
+{
+	remove("t-chflags");
+	remove("t-lchflags");
+	remove("t-lchflags.sym");
+	remove("t-fchflags");
+}
+
+int main()
+{
+	INITIAILIZE_TESTS();
+	CLEANUP(cleanup);
+
+	TEST(test_chflags());
+	TEST(test_lchflags());
+	TEST(test_fchflags());
+
+	VERIFY_RESULT_AND_EXIT();
+}
