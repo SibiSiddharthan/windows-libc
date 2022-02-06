ddfdd20a7716c3662e19f701c4f43257728ba748
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2022-02-06 19:20:23 +0530

    Update copyright to 2022

--- src/signal/internal.c
+++ src/signal/internal.c
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2020-2021 Sibi Siddharthan
+   Copyright (c) 2020-2022 Sibi Siddharthan
 
    Distributed under the MIT license.
    Refer to the LICENSE file at the root directory for details.
