47936f8dd6a76ae2da99c10ab7a607522c82c2e5
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2022-02-22 02:02:28 +0530

    Add mutex timed functions

--- src/thread/mutex.c
+++ src/thread/mutex.c
@@ -10,16 +10,19 @@
 #include <errno.h>
 #include <thread.h>
 
-#define VALIDATE_MUTEX_PTR(mutex) \
-	if (mutex == NULL)            \
-	{                             \
-		errno = EINVAL;           \
-		return -1;                \
+#define VALIDATE_PTR(ptr) \
+	if (ptr == NULL)      \
+	{                     \
+		errno = EINVAL;   \
+		return -1;        \
 	}
+
+#define VALIDATE_MUTEX(mutex)           VALIDATE_PTR(mutex)
+#define VALIDATE_MUTEX_ATTR(mutex_attr) VALIDATE_PTR(mutex_attr)
 
 int wlibc_mutex_init(mutex_t *mutex, const mutex_attr_t *attributes)
 {
-	VALIDATE_MUTEX_PTR(mutex);
+	VALIDATE_MUTEX(mutex);
 
 	NTSTATUS status;
 	HANDLE handle;
@@ -39,7 +42,7 @@ int wlibc_mutex_init(mutex_t *mutex, const mutex_attr_t *attributes)
 
 int wlibc_mutex_destroy(mutex_t *mutex)
 {
-	VALIDATE_MUTEX_PTR(mutex);
+	VALIDATE_MUTEX(mutex);
 
 	NTSTATUS status;
 
@@ -58,26 +61,25 @@ int wlibc_mutex_destroy(mutex_t *mutex)
 
 int wlibc_mutex_lock(mutex_t *mutex)
 {
-	VALIDATE_MUTEX_PTR(mutex);
+	VALIDATE_MUTEX(mutex);
 
 	NTSTATUS status;
 
 	status = NtWaitForSingleObject(mutex->handle, FALSE, NULL);
-
 	if (status != STATUS_SUCCESS)
 	{
 		map_ntstatus_to_errno(status);
 		return -1;
 	}
 
-	mutex->owner = GetCurrentThreadId();
+	_InterlockedExchange((volatile long *)&mutex->owner, GetCurrentThreadId());
 
 	return 0;
 }
 
 int wlibc_mutex_unlock(mutex_t *mutex)
 {
-	VALIDATE_MUTEX_PTR(mutex);
+	VALIDATE_MUTEX(mutex);
 
 	NTSTATUS status;
 
@@ -88,23 +90,66 @@ int wlibc_mutex_unlock(mutex_t *mutex)
 		return -1;
 	}
 
-	mutex->owner = 0;
+	_InterlockedExchange((volatile long *)&mutex->owner, 0);
 
 	return 0;
 }
 
 int wlibc_mutex_trylock(mutex_t *mutex)
 {
-	VALIDATE_MUTEX_PTR(mutex);
+	VALIDATE_MUTEX(mutex);
 
-	// TODO
+	NTSTATUS status;
+	LARGE_INTEGER timeout;
+
+	timeout.QuadPart = 0;
+
+	status = NtWaitForSingleObject(mutex->handle, FALSE, &timeout);
+	if (status != STATUS_SUCCESS && status != STATUS_TIMEOUT)
+	{
+		map_ntstatus_to_errno(status);
+		return -1;
+	}
+
+	if (status == STATUS_TIMEOUT)
+	{
+		errno = EBUSY;
+		return -1;
+	}
+
+	// Mutex has been acquired by the thread.
+	_InterlockedExchange((volatile long *)&mutex->owner, GetCurrentThreadId());
+
 	return 0;
 }
 
 int wlibc_mutex_timedlock(mutex_t *restrict mutex, const struct timespec *restrict abstime)
 {
-	VALIDATE_MUTEX_PTR(mutex);
+	VALIDATE_MUTEX(mutex);
+	VALIDATE_PTR(abstime);
 
-	// TODO
+	NTSTATUS status;
+	LARGE_INTEGER timeout;
+
+	// From utimens.c. TODO
+	timeout.QuadPart = abstime->tv_sec * 10000000 + abstime->tv_nsec / 100;
+	timeout.QuadPart += 116444736000000000LL;
+
+	status = NtWaitForSingleObject(mutex->handle, FALSE, &timeout);
+	if (status != STATUS_SUCCESS && status != STATUS_TIMEOUT)
+	{
+		map_ntstatus_to_errno(status);
+		return -1;
+	}
+
+	if (status == STATUS_TIMEOUT)
+	{
+		errno = EBUSY;
+		return -1;
+	}
+
+	// Mutex has been acquired by the thread.
+	_InterlockedExchange((volatile long *)&mutex->owner, GetCurrentThreadId());
+
 	return 0;
 }
