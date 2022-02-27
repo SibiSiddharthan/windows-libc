4afec26374ef15a457fdc0d7875572b965df4b13
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2022-02-28 01:49:32 +0530

    Change back to using ntdll.lib
    The Github runners don't have ntdllp.lib. Modify the barrier code
    to make use of the kernel32 functions for the time being.

--- src/thread/barrier.c
+++ src/thread/barrier.c
@@ -20,16 +20,30 @@
 #define VALIDATE_BARRIER(barrier)           VALIDATE_PTR(barrier)
 #define VALIDATE_BARRIER_ATTR(barrier_attr) VALIDATE_PTR(barrier_attr)
 
+// NOTE: As of Windows 10 21H2 ntdll.lib does not export the Rtl barrier functions. They are present in ntdllp.lib.
+// The Github hosted runner does not have ntdllp.lib yet. For the time being use the kernel32.lib functions.
+// Once ntdll.lib exports these functions we can switch over to using the Rtl functions.
+
 int wlibc_barrier_init(barrier_t *restrict barrier, const barrier_attr_t *restrict attributes, unsigned int count)
 {
 	VALIDATE_BARRIER(barrier);
 
+#if 0
 	NTSTATUS status;
 
 	status = RtlInitBarrier((PRTL_BARRIER)barrier, count, 0);
 	if (status != STATUS_SUCCESS)
 	{
 		map_ntstatus_to_errno(status);
+		return -1;
+	}
+#endif
+	BOOL status;
+
+	status = InitializeSynchronizationBarrier((PRTL_BARRIER)barrier, count, 0);
+	if (!status)
+	{
+		map_doserror_to_errno(GetLastError());
 		return -1;
 	}
 
@@ -41,14 +55,16 @@ int wlibc_barrier_destroy(barrier_t *barrier)
 	VALIDATE_BARRIER(barrier);
 
 	// This function will not fail.
-	RtlDeleteBarrier((PRTL_BARRIER)barrier);
+	// RtlDeleteBarrier((PRTL_BARRIER)barrier);
+	DeleteSynchronizationBarrier((PRTL_BARRIER)barrier);
 	return 0;
 }
 
 int wlibc_barrier_wait(barrier_t *barrier)
 {
 	VALIDATE_BARRIER(barrier);
-	return RtlBarrier((PRTL_BARRIER)barrier, 0);
+	// return RtlBarrier((PRTL_BARRIER)barrier, 0);
+	return EnterSynchronizationBarrier((PRTL_BARRIER)barrier, 0);
 }
 
 int pthread_barrierattr_init(barrier_attr_t *attributes)
