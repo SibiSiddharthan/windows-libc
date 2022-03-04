491ff9449c85e2d4b3ff9b33291d682266cbc032
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2022-03-05 00:57:39 +0530

    Put the pthread and ISO C11 thread tests into separate directories

---
+++ tests/pthread/test-once.c
@@ -0,0 +1,55 @@
+/*
+   Copyright (c) 2020-2022 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#include <test-macros.h>
+#include <pthread.h>
+
+static int test_variable = 0;
+
+void init_function(void)
+{
+	++test_variable;
+}
+
+void *pthread_function(void *once)
+{
+	pthread_once((pthread_once_t *)once, init_function);
+	return NULL;
+}
+
+int test_once()
+{
+	int status;
+	void *result;
+	pthread_t thread_1, thread_2;
+	pthread_once_t once = PTHREAD_ONCE_INIT;
+
+	status = pthread_create(&thread_1, NULL, pthread_function, (void *)&once);
+	ASSERT_EQ(status, 0);
+
+	status = pthread_create(&thread_2, NULL, pthread_function, (void *)&once);
+	ASSERT_EQ(status, 0);
+
+	status = pthread_join(thread_1, &result);
+	ASSERT_EQ(status, 0);
+	ASSERT_EQ(result, NULL);
+
+	status = pthread_join(thread_2, &result);
+	ASSERT_EQ(status, 0);
+	ASSERT_EQ(result, NULL);
+
+	ASSERT_EQ(test_variable, 1);
+
+	return 0;
+}
+
+int main()
+{
+	INITIAILIZE_TESTS();
+	TEST(test_once());
+	VERIFY_RESULT_AND_EXIT();
+}
---
+++ tests/threads/test-init.c
@@ -0,0 +1,55 @@
+/*
+   Copyright (c) 2020-2022 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#include <test-macros.h>
+#include <threads.h>
+
+static int test_variable = 0;
+
+void init_function(void)
+{
+	++test_variable;
+}
+
+int thrd_function(void *once)
+{
+	call_once((once_flag *)once, init_function);
+	return 1;
+}
+
+int test_init()
+{
+	int status;
+	int result;
+	thrd_t thread_1, thread_2;
+	once_flag once = ONCE_FLAG_INIT;
+
+	status = thrd_create(&thread_1, thrd_function, (void *)&once);
+	ASSERT_EQ(status, 0);
+
+	status = thrd_create(&thread_2, thrd_function, (void *)&once);
+	ASSERT_EQ(status, 0);
+
+	status = thrd_join(thread_1, &result);
+	ASSERT_EQ(status, 0);
+	ASSERT_EQ(result, 1);
+
+	status = thrd_join(thread_2, &result);
+	ASSERT_EQ(status, 0);
+	ASSERT_EQ(result, 1);
+
+	ASSERT_EQ(test_variable, 1);
+
+	return 0;
+}
+
+int main()
+{
+	INITIAILIZE_TESTS();
+	TEST(test_init());
+	VERIFY_RESULT_AND_EXIT();
+}
