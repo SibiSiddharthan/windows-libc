e45be8832619e4ad9b2de3802065b77f2e24f79f
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2022-04-28 01:51:41 +0530

    Introduce MSVC support for building packages with wlibc using CMake

--- cmake/FindWLIBC.cmake
+++ cmake/FindWLIBC.cmake
@@ -5,21 +5,29 @@
    Refer to the LICENSE file at the root directory for details.
 ]]
 
-find_path(WLIBC_INCLUDE_DIR NAMES wlibc-macros.h)
-find_library(WLIBC_LIBRARY NAMES wlibc NAMES_PER_DIR)
-
+include(SelectLibraryConfigurations)
 include(FindPackageHandleStandardArgs)
+
+find_path(WLIBC_INCLUDE_DIR NAMES wlibc.h)
+find_library(WLIBC_LIBRARY_RELEASE NAMES wlibc NAMES_PER_DIR)
+find_library(WLIBC_LIBRARY_DEBUG NAMES wlibcd NAMES_PER_DIR)
+
+select_library_configurations(WLIBC)
 find_package_handle_standard_args(WLIBC REQUIRED_VARS WLIBC_INCLUDE_DIR WLIBC_LIBRARY)
 
 if(WLIBC_FOUND)
-	set(WLIBC_INCLUDE_DIRS ${WLIBC_INCLUDE_DIR})
+	include_directories(BEFORE ${WLIBC_INCLUDE_DIR})
 	set(WLIBC_LIBRARIES ${WLIBC_LIBRARY})
 	if(NOT TARGET WLIBC::WLIBC)
-	add_library(WLIBC::WLIBC UNKNOWN IMPORTED)
-	set_target_properties(WLIBC::WLIBC PROPERTIES
-		INTERFACE_INCLUDE_DIRECTORIES ${WLIBC_INCLUDE_DIRS}
-		INTERFACE_LINK_LIBRARIES ${WLIBC_LIBRARIES}
-		INTERFACE_LINK_OPTIONS -Wl,-entry:wmainCRTStartup #TODO support MSVC
-		IMPORTED_LOCATION ${WLIBC_LIBRARIES})
+		add_library(WLIBC::WLIBC UNKNOWN IMPORTED)
+		set_target_properties(WLIBC::WLIBC PROPERTIES INTERFACE_LINK_OPTIONS "LINKER:/entry:wmainCRTStartup")
+	endif()
+	if(WLIBC_LIBRARY_RELEASE)
+		set_target_properties(WLIBC::WLIBC PROPERTIES IMPORTED_CONFIGURATIONS RELEASE)
+		set_target_properties(WLIBC::WLIBC PROPERTIES IMPORTED_LOCATION_RELEASE ${WLIBC_LIBRARY_RELEASE})
+	endif()
+	if(WLIBC_LIBRARY_DEBUG)
+		set_target_properties(WLIBC::WLIBC PROPERTIES IMPORTED_CONFIGURATIONS DEBUG)
+		set_target_properties(WLIBC::WLIBC PROPERTIES IMPORTED_LOCATION_DEBUG ${WLIBC_LIBRARY_DEBUG})
 	endif()
 endif()
