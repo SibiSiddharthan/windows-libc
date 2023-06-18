f7b029c85ffe41a638c0bbef0e4f3a1635ab19fd
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2023-06-18 11:44:22 +0530

    Update copyright to 2023

--- include/process.h
+++ include/process.h
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2020-2022 Sibi Siddharthan
+   Copyright (c) 2020-2023 Sibi Siddharthan
 
    Distributed under the MIT license.
    Refer to the LICENSE path at the root directory for details.
