e2776018950998e8d0f671c5c8c7aada9f8d13fe
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2023-07-14 11:27:57 +0530

    Replace string.h

---
+++ include/string.h
@@ -0,0 +1,173 @@
+/*
+   Copyright (c) 2020-2023 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#ifndef WLIBC_STRING_EXT_H
+#define WLIBC_STRING_EXT_H
+
+#include <wlibc.h>
+#include <corecrt_memory.h>
+
+#define _NLSCMPERROR _CRT_INT_MAX // currently == INT_MAX
+
+/* String manipulation */
+
+_ACRTIMP char *__cdecl strcat(char *destination, char const *source);
+_ACRTIMP char *__cdecl strncat(char *destination, char const *source, size_t count);
+_ACRTIMP errno_t __cdecl strcat_s(char *dest, rsize_t size, char const *src);
+_ACRTIMP errno_t __cdecl strncat_s(char *dest, rsize_t size, char const *src, rsize_t maxsize);
+
+_ACRTIMP char *__cdecl strcpy(char *destination, char const *source);
+_ACRTIMP char *__cdecl strncpy(char *destination, char const *source, size_t count);
+_ACRTIMP errno_t __cdecl strcpy_s(char *dest, rsize_t size, char const *src);
+_ACRTIMP errno_t __cdecl strncpy_s(char *dest, rsize_t size, char const *src, rsize_t maxsize);
+
+_ACRTIMP char *__cdecl strlwr(char *str);
+_ACRTIMP char *__cdecl _strlwr(char *str);
+_ACRTIMP errno_t __cdecl _strlwr_s(char *str, size_t size);
+_ACRTIMP errno_t __cdecl _strlwr_s_l(char *str, size_t size, _locale_t locale);
+
+_ACRTIMP char *__cdecl strupr(char *str);
+_ACRTIMP char *__cdecl _strupr(char *str);
+_ACRTIMP errno_t __cdecl _strupr_s(char *str, size_t size);
+_ACRTIMP errno_t __cdecl _strupr_s_l(char *str, size_t size, _locale_t locale);
+
+_ACRTIMP char *__cdecl strset(char *str, int value);
+_ACRTIMP char *__cdecl strnset(char *str, int value, size_t count);
+_ACRTIMP char *__cdecl _strset(char *str, int value);
+_ACRTIMP char *__cdecl _strnset(char *str, int value, size_t count);
+_ACRTIMP errno_t __cdecl _strset_s(char *destination, size_t destination_size, int value);
+_ACRTIMP errno_t __cdecl _strnset_s(char *str, size_t size, int value, size_t count);
+
+_ACRTIMP char *__cdecl strrev(char *str);
+_ACRTIMP char *__cdecl _strrev(char *str);
+
+_ACRTIMP size_t __cdecl strxfrm(char *destination, char const *source, size_t count);
+_ACRTIMP size_t __cdecl _strxfrm_l(char *destination, char const *source, size_t count, _locale_t locale);
+
+/* String examination */
+
+_ACRTIMP size_t __cdecl strlen(char const *str);
+_ACRTIMP size_t __cdecl strnlen(char const *str, size_t count);
+static __inline size_t __CRTDECL strnlen_s(char const *str, size_t size)
+{
+	return str == NULL ? 0 : strnlen(str, size);
+}
+
+_ACRTIMP int __cdecl strcmp(char const *str1, char const *str2);
+_ACRTIMP int __cdecl stricmp(char const *str1, char const *str2);
+_ACRTIMP int __cdecl _stricmp(char const *str1, char const *str2);
+_ACRTIMP int __cdecl _stricmp_l(char const *str1, char const *str2, _locale_t locale);
+
+_ACRTIMP int __cdecl strncmp(char const *str1, char const *str2, size_t count);
+_ACRTIMP int __cdecl strnicmp(char const *str1, char const *str2, size_t count);
+_ACRTIMP int __cdecl _strnicmp(char const *str1, char const *str2, size_t count);
+_ACRTIMP int __cdecl _strnicmp_l(char const *str1, char const *str2, size_t count, _locale_t locale);
+
+_ACRTIMP int __cdecl strcmpi(char const *str1, char const *str2);
+_ACRTIMP int __cdecl _strcmpi(char const *str1, char const *str2);
+
+_ACRTIMP int __cdecl strcoll(char const *str1, char const *str2);
+_ACRTIMP int __cdecl _strcoll_l(char const *str1, char const *str2, _locale_t locale);
+_ACRTIMP int __cdecl _stricoll(char const *str1, char const *str2);
+_ACRTIMP int __cdecl _stricoll_l(char const *str1, char const *str2, _locale_t locale);
+
+_ACRTIMP int __cdecl _strncoll(char const *str1, char const *str2, size_t count);
+_ACRTIMP int __cdecl _strncoll_l(char const *str1, char const *str2, size_t count, _locale_t locale);
+_ACRTIMP int __cdecl _strnicoll(char const *str1, char const *str2, size_t count);
+_ACRTIMP int __cdecl _strnicoll_l(char const *str1, char const *str2, size_t count, _locale_t locale);
+
+_ACRTIMP size_t __cdecl strspn(char const *str, char const *control);
+_ACRTIMP size_t __cdecl strcspn(char const *str, char const *control);
+
+_ACRTIMP char *__cdecl strpbrk(char const *str, char const *control);
+
+_ACRTIMP char *__cdecl strtok(_Inout_opt_ char *str, char const *delimiter);
+_ACRTIMP char *__cdecl strtok_s(char *str, char const *delimiter, char **context);
+
+_ACRTIMP size_t __cdecl __strncnt(char const *str, size_t count);
+
+#if defined _DEBUG && defined _CRTDBG_MAP_ALLOC
+#	pragma push_macro("_strdup")
+#	pragma push_macro("strdup")
+#	undef _strdup
+#	undef strdup
+#endif
+
+_ACRTIMP _CRTALLOCATOR char *__cdecl _strdup(char const *source);
+_ACRTIMP _CRTALLOCATOR char *__cdecl strdup(char const *source);
+
+#if defined _DEBUG && defined _CRTDBG_MAP_ALLOC
+#	pragma pop_macro("strdup")
+#	pragma pop_macro("_strdup")
+#endif
+
+/* Multibyte string functions */
+#define _FILE_DEFINED
+#include <mbstring.h>
+
+/* Extensions */
+_WLIBC_BEGIN_DECLS
+
+WLIBC_API char *wlibc_common_strerror(int errnum, _locale_t locale);
+
+WLIBC_INLINE char *strerror(int errnum)
+{
+	return wlibc_common_strerror(errnum, NULL);
+}
+
+#pragma warning(push)
+#pragma warning(disable : 4100) // Unused parameter
+
+WLIBC_INLINE char *strerror_r(int errnum, char *buffer WLIBC_UNUSED, size_t length WLIBC_UNUSED)
+{
+	return wlibc_common_strerror(errnum, NULL);
+}
+
+#pragma warning(pop)
+
+WLIBC_INLINE char *strerror_l(int errnum, _locale_t locale)
+{
+	return wlibc_common_strerror(errnum, locale);
+}
+
+
+WLIBC_INLINE void *mempcpy(void *dest, const void *src, size_t size)
+{
+	return (void *)((char *)memcpy(dest, src, size) + size);
+}
+
+WLIBC_API void *wlibc_memrchr(const void *str, int character, size_t size);
+WLIBC_INLINE void *memrchr(const void *str, int character, size_t size)
+{
+	return (void *)wlibc_memrchr(str, character, size);
+}
+
+WLIBC_API void *wlibc_rawmemchr(const void *str, int character);
+WLIBC_INLINE void *rawmemchr(const void *str, int character)
+{
+	return wlibc_rawmemchr(str, character);
+}
+
+WLIBC_API char *wlibc_stpcpy(char *dest, const char *src);
+WLIBC_API char *wlibc_stpncpy(char *dest, const char *src, size_t size);
+
+WLIBC_INLINE char *stpcpy(char *dest, const char *src)
+{
+	return wlibc_stpcpy(dest, src);
+}
+
+WLIBC_INLINE char *stpncpy(char *dest, const char *src, size_t size)
+{
+	return wlibc_stpncpy(dest, src, size);
+}
+
+_WLIBC_END_DECLS
+
+/* Extra stuff */
+#include <strings.h>
+
+#endif
--- include/wchar.h
+++ include/wchar.h
@@ -7,37 +7,34 @@
 
 /*
  * MSVC's wchar.h is huge and include a whole bunch of uneccesary header files like corecrt_wdirect.h, corecrt_wstdlib.h, etc.
- * This is just a stub compatibility header. No functions in this file will be implemented.
+ * This is just a stub compatibility header.
  */
 
 #ifndef WLIBC_WCHAR_H
 #define WLIBC_WCHAR_H
 
 #include <wlibc.h>
-#include <corecrt.h>
 #include <vcruntime_string.h>
 
 #define WCHAR_MIN 0x0000
 #define WCHAR_MAX 0xffff
 
-/* Let's just stick with _ACRTIMP for linkage */
-
 _ACRTIMP wint_t __cdecl btowc(int ch);
 _ACRTIMP size_t __cdecl mbrlen(char const *ch, size_t bytes, mbstate_t *state);
-_ACRTIMP size_t __cdecl mbrtowc(wchar_t *dest, char const *src, size_t bytes, mbstate_t *state);
-_ACRTIMP size_t __cdecl mbsrtowcs(wchar_t *dest, char const **source, size_t count, mbstate_t *state);
-_ACRTIMP size_t __cdecl wcrtomb(char *dest, wchar_t source, mbstate_t *state);
-_ACRTIMP size_t __cdecl wcsrtombs(char *dest, wchar_t const **source, size_t count, mbstate_t *state);
+_ACRTIMP size_t __cdecl mbrtowc(wchar_t *destination, char const *src, size_t bytes, mbstate_t *state);
+_ACRTIMP size_t __cdecl mbsrtowcs(wchar_t *destination, char const **source, size_t count, mbstate_t *state);
+_ACRTIMP size_t __cdecl wcrtomb(char *destination, wchar_t source, mbstate_t *state);
+_ACRTIMP size_t __cdecl wcsrtombs(char *destination, wchar_t const **source, size_t count, mbstate_t *state);
 _ACRTIMP int __cdecl wctob(wint_t wch);
 
 /* Inline functions */
 
-WLIBC_INLINE int __cdecl mbsinit(mbstate_t const *p)
+static __inline int __cdecl mbsinit(mbstate_t const *p)
 {
 	return p == NULL || p->_Wchar == 0;
 }
 
-WLIBC_INLINE wchar_t *__cdecl wmemchr(wchar_t const *wstr, wchar_t wch, size_t n)
+static __inline wchar_t *__cdecl wmemchr(wchar_t const *wstr, wchar_t wch, size_t n)
 {
 	for (; 0 < n; ++wstr, --n)
 		if (*wstr == wch)
@@ -46,7 +43,7 @@ WLIBC_INLINE wchar_t *__cdecl wmemchr(wchar_t const *wstr, wchar_t wch, size_t n
 	return 0;
 }
 
-WLIBC_INLINE int __cdecl wmemcmp(wchar_t const *wstr1, wchar_t const *wstr2, size_t count)
+static __inline int __cdecl wmemcmp(wchar_t const *wstr1, wchar_t const *wstr2, size_t count)
 {
 	for (; 0 < count; ++wstr1, ++wstr2, --count)
 		if (*wstr1 != *wstr2)
@@ -55,17 +52,17 @@ WLIBC_INLINE int __cdecl wmemcmp(wchar_t const *wstr1, wchar_t const *wstr2, siz
 	return 0;
 }
 
-WLIBC_INLINE wchar_t *__cdecl wmemcpy(wchar_t *dest, wchar_t const *source, size_t count)
+static __inline wchar_t *__cdecl wmemcpy(wchar_t *destination, wchar_t const *source, size_t count)
 {
-	return (wchar_t *)memcpy(dest, source, count * sizeof(wchar_t));
+	return (wchar_t *)memcpy(destination, source, count * sizeof(wchar_t));
 }
 
-WLIBC_INLINE wchar_t *__cdecl wmemmove(wchar_t *dest, wchar_t const *source, size_t count)
+static __inline wchar_t *__cdecl wmemmove(wchar_t *destination, wchar_t const *source, size_t count)
 {
-	return (wchar_t *)memmove(dest, source, count * sizeof(wchar_t));
+	return (wchar_t *)memmove(destination, source, count * sizeof(wchar_t));
 }
 
-WLIBC_INLINE wchar_t *__cdecl wmemset(wchar_t *wstr, wchar_t wch, size_t count)
+static __inline wchar_t *__cdecl wmemset(wchar_t *wstr, wchar_t wch, size_t count)
 {
 	wchar_t *temp = wstr;
 	for (; 0 < count; ++temp, --count)
@@ -77,12 +74,97 @@ WLIBC_INLINE wchar_t *__cdecl wmemset(wchar_t *wstr, wchar_t wch, size_t count)
 
 #if __STDC_WANT_SECURE_LIB__
 
-_ACRTIMP errno_t __cdecl mbsrtowcs_s(size_t *retval, wchar_t *dest, size_t size, char const **source, size_t _N, mbstate_t *state);
-_ACRTIMP errno_t __cdecl wcrtomb_s(size_t *retval, char *dest, size_t bytes, wchar_t ch, mbstate_t *state);
-_ACRTIMP errno_t __cdecl wcsrtombs_s(size_t *retval, char *dest, size_t bytes, wchar_t const **source, size_t size, mbstate_t *state);
-_ACRTIMP errno_t __cdecl wmemcpy_s(wchar_t *dest, rsize_t destsz, wchar_t const *source, rsize_t count);
-_ACRTIMP errno_t __cdecl wmemmove_s(wchar_t *dest, rsize_t destsz, wchar_t const *source, rsize_t count);
+_ACRTIMP errno_t __cdecl mbsrtowcs_s(size_t *retval, wchar_t *destination, size_t size, char const **source, size_t count,
+									 mbstate_t *state);
+_ACRTIMP errno_t __cdecl wcrtomb_s(size_t *retval, char *destination, size_t bytes, wchar_t ch, mbstate_t *state);
+_ACRTIMP errno_t __cdecl wcsrtombs_s(size_t *retval, char *destination, size_t bytes, wchar_t const **source, size_t size,
+									 mbstate_t *state);
+_ACRTIMP errno_t __cdecl wmemcpy_s(wchar_t *destination, rsize_t destinationsz, wchar_t const *source, rsize_t count);
+_ACRTIMP errno_t __cdecl wmemmove_s(wchar_t *destination, rsize_t destinationsz, wchar_t const *source, rsize_t count);
 
+#endif
+
+/* Wide string functions */
+
+_ACRTIMP wchar_t *__cdecl wcscat(wchar_t *destination, wchar_t const *source);
+_ACRTIMP wchar_t *__cdecl wcsncat(wchar_t *destination, wchar_t const *source, size_t count);
+_ACRTIMP errno_t __cdecl wcscat_s(wchar_t *destination, rsize_t size, wchar_t const *source);
+_ACRTIMP errno_t __cdecl wcsncat_s(wchar_t *destination, rsize_t size, wchar_t const *source, rsize_t count);
+
+_ACRTIMP wchar_t *__cdecl wcscpy(wchar_t *destination, wchar_t const *source);
+_ACRTIMP wchar_t *__cdecl wcsncpy(wchar_t *destination, wchar_t const *source, size_t count);
+_ACRTIMP errno_t __cdecl wcscpy_s(wchar_t *destination, rsize_t size, wchar_t const *source);
+_ACRTIMP errno_t __cdecl wcsncpy_s(wchar_t *destination, rsize_t size, wchar_t const *source, rsize_t count);
+
+_ACRTIMP wchar_t *__cdecl wcslwr(wchar_t *str);
+_ACRTIMP wchar_t *__cdecl _wcslwr(wchar_t *str);
+_ACRTIMP errno_t __cdecl _wcslwr_s(wchar_t *str, size_t size);
+_ACRTIMP errno_t __cdecl _wcslwr_s_l(wchar_t *str, size_t size, _locale_t _Locale);
+
+_ACRTIMP wchar_t *__cdecl wcsupr(wchar_t *str);
+_ACRTIMP wchar_t *__cdecl _wcsupr(wchar_t *str);
+_ACRTIMP errno_t __cdecl _wcsupr_s(wchar_t *str, size_t size);
+_ACRTIMP errno_t __cdecl _wcsupr_s_l(wchar_t *str, size_t size, _locale_t _Locale);
+
+_ACRTIMP wchar_t *__cdecl wcsset(wchar_t *str, wchar_t value);
+_ACRTIMP wchar_t *__cdecl _wcsset(wchar_t *str, wchar_t value);
+_ACRTIMP wchar_t *__cdecl wcsnset(wchar_t *str, wchar_t value, size_t count);
+_ACRTIMP wchar_t *__cdecl _wcsnset(wchar_t *str, wchar_t value, size_t count);
+_ACRTIMP errno_t __cdecl _wcsset_s(wchar_t *destination, size_t size, wchar_t value);
+_ACRTIMP errno_t __cdecl _wcsnset_s(wchar_t *destination, size_t size, wchar_t value, size_t count);
+
+_ACRTIMP wchar_t *__cdecl wcsrev(wchar_t *str);
+_ACRTIMP wchar_t *__cdecl _wcsrev(wchar_t *str);
+
+_ACRTIMP size_t __cdecl wcsxfrm(wchar_t *destination, wchar_t const *source, size_t count);
+_ACRTIMP size_t __cdecl _wcsxfrm_l(wchar_t *destination, wchar_t const *source, size_t count, _locale_t _Locale);
+
+_ACRTIMP size_t __cdecl wcslen(wchar_t const *str);
+_ACRTIMP size_t __cdecl wcsnlen(wchar_t const *source, size_t count);
+
+_ACRTIMP int __cdecl wcscmp(wchar_t const *str1, wchar_t const *str2);
+_ACRTIMP int __cdecl wcsncmp(wchar_t const *str1, wchar_t const *str2, size_t count);
+_ACRTIMP int __cdecl _wcsicmp(wchar_t const *str1, wchar_t const *str2);
+_ACRTIMP int __cdecl _wcsnicmp(wchar_t const *str1, wchar_t const *str2, size_t count);
+_ACRTIMP int __cdecl _wcsicmp_l(wchar_t const *str1, wchar_t const *str2, _locale_t _Locale);
+_ACRTIMP int __cdecl _wcsnicmp_l(wchar_t const *str1, wchar_t const *str2, size_t count, _locale_t _Locale);
+
+_ACRTIMP int __cdecl wcsicmp(wchar_t const *str1, wchar_t const *str2);
+_ACRTIMP int __cdecl wcsnicmp(wchar_t const *str1, wchar_t const *str2, size_t count);
+
+_ACRTIMP int __cdecl wcscoll(wchar_t const *str1, wchar_t const *str2);
+_ACRTIMP int __cdecl _wcscoll_l(wchar_t const *str1, wchar_t const *str2, _locale_t _Locale);
+_ACRTIMP int __cdecl _wcsicoll(wchar_t const *str1, wchar_t const *str2);
+_ACRTIMP int __cdecl _wcsicoll_l(wchar_t const *str1, wchar_t const *str2, _locale_t _Locale);
+
+_ACRTIMP int __cdecl _wcsncoll(wchar_t const *str1, wchar_t const *str2, size_t count);
+_ACRTIMP int __cdecl _wcsncoll_l(wchar_t const *str1, wchar_t const *str2, size_t count, _locale_t _Locale);
+_ACRTIMP int __cdecl _wcsnicoll(wchar_t const *str1, wchar_t const *str2, size_t count);
+_ACRTIMP int __cdecl _wcsnicoll_l(wchar_t const *str1, wchar_t const *str2, size_t count, _locale_t _Locale);
+
+_ACRTIMP int __cdecl wcsicoll(wchar_t const *str1, wchar_t const *str2);
+
+_ACRTIMP size_t __cdecl wcscspn(wchar_t const *str, wchar_t const *_Control);
+_ACRTIMP size_t __cdecl wcsspn(wchar_t const *str, wchar_t const *_Control);
+
+_ACRTIMP wchar_t _CONST_RETURN *__cdecl wcspbrk(wchar_t const *str, wchar_t const *_Control);
+
+_ACRTIMP wchar_t *__cdecl wcstok(wchar_t *str, wchar_t const *delimiter, wchar_t **context);
+_ACRTIMP wchar_t *__cdecl wcstok_s(wchar_t *str, wchar_t const *delimiter, wchar_t **context);
+
+#if defined _DEBUG && defined _CRTDBG_MAP_ALLOC
+#	pragma push_macro("_wcsdup")
+#	pragma push_macro("wcsdup")
+#	undef _wcsdup
+#	undef wcsdup
+#endif
+
+_ACRTIMP _CRTALLOCATOR wchar_t *__cdecl _wcsdup(wchar_t const *str);
+_ACRTIMP _CRTALLOCATOR wchar_t *__cdecl wcsdup(wchar_t const *str);
+
+#if defined _DEBUG && defined _CRTDBG_MAP_ALLOC
+#	pragma pop_macro("wcsdup")
+#	pragma pop_macro("_wcsdup")
 #endif
 
 /* Extensions */
